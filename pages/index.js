ef9422fe87cdcd69cafc0bb672f171727762d88b
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2022-06-14 15:27:03 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Container, Box } from "@chakra-ui/react"
+
+const Page = () =>{
+    return (
+        <Container><Box borderRadius="lg" bg="red" p={3} align="center">
+            Hello, I&apos;m a full-stack cloud developer based in Tunisia 
+            </Box>
+            </Container>
+        
+    )}
+export default Page
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options = {
+    arrowParens: 'avoid',
+    signleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWidth: 2,
+    trailingComma: 'none'
+}
+module.exports = options
